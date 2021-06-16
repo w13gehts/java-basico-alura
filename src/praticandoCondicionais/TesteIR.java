13d1a16c3b749570abfbd3685b754a12b6754ddc
Juliana <juliana.salafia@gmail.com>
2021-06-16 11:50:56 -0300

    Otimização do exercício de cobrança sobre o Imposto de Renda

--- src/praticandoCondicionais/TesteIR.java
+++ src/praticandoCondicionais/TesteIR.java
@@ -1,5 +1,4 @@
 package praticandoCondicionais;
-
 import java.util.Scanner;
 
 public class TesteIR {
@@ -8,30 +7,40 @@ public class TesteIR {
         System.out.println("Verificação do Imposto de Renda");
 
         double salario;
+        double aliquota;
 
         try (Scanner scanner = new Scanner(System.in)) {
             System.out.println("Digite o valor do salário: ");
             salario = scanner.nextDouble();
         }
 
-            if (salario >= 1903.99 && salario <= 2826.65) {
-                salario = salario - 142.80;
-                System.out.println("A dedução na declaração do imposto de renda é de 7.5%, no valor de R$142.80. " +
-                        "O salário líquido é de " + salario);
+        boolean categoriaA = salario >= 1903.99 && salario <= 2826.65;
+        boolean categoriaB = salario >= 2826.66 && salario <= 3751.05;
+        boolean categoriaC = salario >= 3751.06 && salario <= 4664.68;
+        boolean categoriaD = salario >= 4664.68;
 
-            } else if (salario >= 2826.66 && salario <= 3751.05) {
-                salario = salario - 350.80;
-                System.out.println("A dedução na declaração do imposto de renda é de 15%, no valor de R$350.80. " +
-                        "O salário líquido é de " + salario);
+           if (categoriaA) {
+                aliquota = 142.80;
+                salario = salario - aliquota;
+                System.out.println("A dedução na declaração do imposto de renda é de 7.5%, no valor de R$"
+                        + aliquota + ". O salário líquido é de R$" + salario);
 
-            } else if (salario >= 3751.06 && salario <= 4664.68){
-                salario = salario - 636.13;
-            System.out.println("A dedução na declaração do imposto de renda é de 22.5%, no valor de R$636.13. " +
-                    "O salário líquido é de " + salario);
-            } else if (salario >= 4664.68) {
-                salario = salario - 869.36;
-                System.out.println("A dedução na declaração do imposto de renda é de 27.5%, no valor de R$869.36. " +
-                        "O salário líquido é de " + salario);
+            } else if (categoriaB) {
+                aliquota = 354.80;
+                salario = salario - aliquota;
+                System.out.println("A dedução na declaração do imposto de renda é de 15%, no valor de R$"
+                        + aliquota + ". O salário líquido é de R$" + salario);
+
+            } else if (categoriaC){
+                aliquota = 636.13;
+                salario = salario - aliquota;
+            System.out.println("A dedução na declaração do imposto de renda é de 22.5%, no valor de R$"
+                    + aliquota + ". O salário líquido é de R$" + salario);
+            } else if (categoriaD) {
+                aliquota = 869.36;
+                salario = salario - aliquota;
+                System.out.println("A dedução na declaração do imposto de renda é de 27.5%, no valor de R$"
+                        + aliquota + ". O salário líquido é de R$" + salario);
             } else {
                 System.out.println("O valor de R$" + salario + " é isento do IR");
             }
