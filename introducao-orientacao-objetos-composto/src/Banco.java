ee991432cd8c299f8ec4af18531e367677dd03b6
Juliana <juliana.salafia@gmail.com>
2021-06-20 22:27:09 -0300

    Reorganização do código e comentários.

--- introducao-orientacao-objetos-composto/src/Banco.java
+++ introducao-orientacao-objetos-composto/src/Banco.java
@@ -1,15 +1,20 @@
-public class TestaBanco {
+public class Banco {
 
     public static void main(String[] args) {
+
+        //cria o cliente paulo e popula os dados
         Cliente paulo = new Cliente();
         paulo.nome = "Paulo Silveira";
         paulo.cpf = "222.222.222-22";
         paulo.profissao = "programador";
 
+        //cria uma conta para o paulo e deposita um valor
         Conta contaDoPaulo = new Conta();
         contaDoPaulo.deposita(100);
 
+        //associa o cliente Paulo à conta do Paulo
         contaDoPaulo.titular = paulo;
+
         System.out.println(contaDoPaulo.titular.nome);
         System.out.println(contaDoPaulo.titular);
         System.out.println(paulo);
--- introducao-orientacao-objetos-composto/src/ContaSemCliente.java
+++ introducao-orientacao-objetos-composto/src/ContaSemCliente.java
@@ -1,13 +1,15 @@
-public class TestaContaSemCliente {
+public class ContaSemCliente {
 
     public static void main(String[] args) {
-
+        //cria uma conta e imprime o saldo
         Conta contaDaMarcela = new Conta();
         System.out.println(contaDaMarcela.getSaldo());
 
+        //o titular da conta é um novo cliente
         contaDaMarcela.titular = new Cliente();
         System.out.println(contaDaMarcela.titular);
 
+        //define o nome do titular da conta
         contaDaMarcela.titular.nome = "Marcela";
         System.out.println(contaDaMarcela.titular.nome);
     }
